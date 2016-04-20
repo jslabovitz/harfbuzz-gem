e85f1b509cd9e8d64e8b5961b52069d33e99dce4
John Labovitz <johnl@johnlabovitz.com>
2016-04-20 11:05:49 -0400

    Fix typo in initializer.

--- lib/harfbuzz/blob.rb
+++ lib/harfbuzz/blob.rb
@@ -25,7 +25,7 @@ module Harfbuzz
 
     attr_reader :hb_blob
 
-    def initialize(input, mode=0, user_data=nil)
+    def initialize(data, mode=0, user_data=nil)
       data = data.read if data.kind_of?(IO)
       data_ptr = FFI::MemoryPointer.new(:char, data.size)
       data_ptr.put_bytes(0, data)
